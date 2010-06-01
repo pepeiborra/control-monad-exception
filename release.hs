5d8c2c9b3a6520c093c63c22dcfb2c096d376904
José Iborra <pepe@vera2g13-7.wi-fi.upv.es>
2010-06-01 16:40:30 +0200

    updates to the release script

--- release.hs
+++ release.hs
@@ -25,25 +25,32 @@ descriptors = ["control-monad-exception.cabal"
 
 releaseDir = "release"
 
-data Action = Release | Build | Script deriving Eq
+data Action = Release | Test | Script deriving Eq
+
+isRelease Release = True
+isRelease Script  = True
+isRelease Test    = False
+
+isScript  Script  = True
+isScript  _       = False
 
 main = do
   args   <- getArgs
   action <- case args of
               [] -> return Script
-              ["build"] -> return Build
+              ["test"] -> return Test
               ["really", "release"] -> return Release
               _ -> do
-                putStrLn "USAGE: release [build|really release]"
+                putStrLn "USAGE: release [test|really release]"
                 exitWith ExitSuccess
 
-  let real = action /= Script
+  let real = not . isScript $ action
 -- Auxiliary functions
   let cmd = case action of
             Release-> \cmd -> do {putStrLn cmd; system cmd}
-            Build  -> \cmd -> do {putStrLn cmd; system cmd}
+            Test   -> \cmd -> do {putStrLn cmd; system cmd}
             Script -> \cmd -> do {putStrLn cmd; return ExitSuccess}
-      rename f f' = do
+      mv f f' = do
          putStrLn ("mv " ++ f ++ " " ++ f')
          when real $ renameFile f f'
 
@@ -63,29 +70,43 @@ main = do
          putStrLn ("mkDir " ++ d)
          when real $ createDirectoryIfMissing True d
 
+      stripExtension f = mv f (dropExtension f)
+      cpTemp f = go [] where
+         go tag = do
+           let f' = f <.> "tmp" ++ if null tag then "" else show (head tag)
+           exists <- doesFileExist f'
+           if not exists then copy f f' *> pure f'
+              else go (inc tag)
+         inc [] = [1]
+         inc [x] = [x+1]
+
       saveCurrentCabalFiles = do
          currentDescriptors <- filter (\x -> takeExtension x ==".cabal")
                            <$> (getDirectoryContents =<< getCurrentDirectory)
-         currentDescriptorsContents <- mapM BS.readFile currentDescriptors
-         mapM (\x -> unless (x `elem` descriptors) $ rm x) currentDescriptors
-         return (mapM (uncurry BS.writeFile) (zip currentDescriptors currentDescriptorsContents))
+         currentDescriptorsTmp <- mapM cpTemp currentDescriptors
+         return (mapM stripExtension currentDescriptorsTmp)
 
--- | Build, test, package and store for release.
-      cabal_dist d = do
-        _ <- cmd "cabal clean"
-        guardOk (d ++ " failed to build correctly")   =<< cmd "cabal install"
+-- | build and test
+      cabal_test d = do
+        _ <- cmd "cabal clean -v0"
+        guardOk (d ++ " failed to build correctly")   =<< cmd "cabal install -v0"
         guardOk (d ++ " failed to test correctly")    =<< cmd "cabal test"
+
+-- | package and store for release.
+      cabal_dist d = when (isRelease action) $ do
         guardOk (d ++ " failed to package correctly") =<< cmd ("cabal sdist --builddir=" ++ releaseDir)
 
--- | Upload a package already stored in the release dir to Hackage
+-- | Returns an action to upload a package already stored in the release dir to Hackage
       cabal_upload d = do
-           version <- showVersion . pkgVersion . package . packageDescription
+        version <- showVersion . pkgVersion . package . packageDescription
                   <$> readPackageDescription Verbosity.normal d
-           let packagedFilePath = (dropExtension.dropExtension) d ++ "-" ++ version <.> "tar.gz"
-
-           guardOk (packagedFilePath ++ " failed to upload correctly to Hackage") =<<
-               cmd ("cabal upload " ++ releaseDir </> packagedFilePath)
-           rm (releaseDir </> packagedFilePath)
+        let packagedFilePath = (dropExtension.dropExtension) d ++ "-" ++ version <.> "tar.gz"
+        return $
+         if isRelease action
+          then toOk (packagedFilePath ++ " failed to upload correctly to Hackage") <$>
+                  cmd ("cabal upload " ++ releaseDir </> packagedFilePath)
+               <* rm (releaseDir </> packagedFilePath)
+          else return Ok
 
 -- THE ACTUAL SCRIPT
 
@@ -95,19 +116,24 @@ main = do
     uploadActions <- forM descriptors $ \d -> do
       exists <- doesFileExist d
       let ext = takeExtension d
-      if not exists
-        then return(return ())
-        else case ext of
-        ".cabal" -> pure (cabal_upload d) <* cabal_dist d <* rm d
-        ".pp" -> do
-           let d' = dropExtension d
-           copy d d'
-           cabal_dist d'
-           return $ cabal_upload d
+      if exists || isScript action
+        then cabal_upload d <*
+             case ext of
+               ".cabal" -> cabal_test d <* cabal_dist d <* rm d
+               ".pp" -> let d' = dropExtension d in
+                        copy d d' <* cabal_test d' <* cabal_dist d' <* rm d'
+        else pure (pure (Fail d))
 
-    unless (action == Build) $ do
-      released <- length `liftM` sequence uploadActions
-      when (action == Release) $ putStrLn ("Release of " ++ show released ++ " packages completed.")
+
+    unless (isScript action) $ putStrLn "Packages tested succesfully"
+    when (isRelease action) $ do
+      done <- sequence uploadActions
+      unless (isScript action) $ do
+          putStrLn ("Release of " ++ show (length $ filter isOk done) ++ " packages completed.")
+          when (not $ all isOk done) $ do
+               putStrLn $ "Warning: there were " ++ show(length$ filter (not.isOk) done)
+                            ++ " packages which failed to release"
+               forM_ (filter (not.isOk) done) $ \(Fail msg) -> putStrLn msg
       rmDir releaseDir
 
 ignore _ = return ()
@@ -117,3 +143,10 @@ guardOk msg (ExitFailure _) = do
   exitWith (ExitFailure 1)
 
 guardOk _ ExitSuccess = return ()
+
+data Ok = Ok | Fail {msg::String} deriving (Eq,Ord,Show)
+toOk _   ExitSuccess = Ok
+toOk msg _           = Fail msg
+
+isOk Ok = True
+isOk _  = False
